f10d117fbff607487a8363819bd74d80273e142c
Camille LE LUET <cleluet@infinitesquare.com>
2019-03-25 17:20:09 +0100

    sample

---
+++ GradientViewSample/GradientViewSample.Android/Renderers/DroidGradientViewRenderer.cs
@@ -0,0 +1,79 @@
+﻿using Android.Content;
+using Android.Graphics.Drawables;
+using GradientViewSample.Controls;
+using GradientViewSample.Droid.Renderers;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Color = Xamarin.Forms.Color;
+
+[assembly: Xamarin.Forms.ExportRenderer(typeof(GradientView), typeof(DroidGradientViewRenderer))]
+namespace GradientViewSample.Droid.Renderers
+{
+    public class DroidGradientViewRenderer : VisualElementRenderer<ContentView>
+    {
+        public DroidGradientViewRenderer(Context context)
+            : base(context)
+        {
+        }
+
+        private Color StartColor { get; set; }
+        private Color EndColor { get; set; }
+        private StackOrientation Orientation { get; set; }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<ContentView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null)
+            {
+                return;
+            }
+
+            try
+            {
+                var stack = (GradientView)e.NewElement;
+                StartColor = stack.StartColor;
+                EndColor = stack.EndColor;
+                Orientation = stack.Orientation;
+
+                SetGradientBackground();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(@"ERROR:", ex.Message);
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (e.PropertyName == nameof(GradientView.StartColor) || e.PropertyName == nameof(GradientView.EndColor))
+            {
+                var stack = (GradientView)sender;
+                if (stack == null)
+                {
+                    return;
+                }
+
+                StartColor = stack.StartColor;
+                EndColor = stack.EndColor;
+                Orientation = stack.Orientation;
+
+                SetGradientBackground();
+            }
+        }
+
+        private void SetGradientBackground()
+        {
+            int[] colors = { StartColor.ToAndroid(), EndColor.ToAndroid() };
+            GradientDrawable gradient = new GradientDrawable(
+                Orientation == StackOrientation.Horizontal
+                ? GradientDrawable.Orientation.LeftRight
+                : GradientDrawable.Orientation.TopBottom,
+                colors);
+            Android.Support.V4.View.ViewCompat.SetBackground(this, gradient);
+        }
+    }
+}
---
+++ GradientViewSample/GradientViewSample.iOS/Renderers/IOSGradientViewRenderer.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using CoreAnimation;
+using CoreGraphics;
+using GradientViewSample.iOS.Renderers;
+using GradientViewSample.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(GradientView), typeof(IOSGradientViewRenderer))]
+namespace GradientViewSample.iOS.Renderers
+{
+    public class IOSGradientViewRenderer : VisualElementRenderer<ContentView>
+    {
+        public override void Draw(CGRect rect)
+        {
+            base.Draw(rect);
+            GradientView layout = (GradientView)Element;
+
+            CGColor startColor = layout.StartColor.ToCGColor();
+            CGColor endColor = layout.EndColor.ToCGColor();
+            StackOrientation orientation = layout.Orientation;
+
+            CAGradientLayer gradientLayer = new CAGradientLayer();
+            if (orientation == StackOrientation.Horizontal)
+            {
+                gradientLayer = new CAGradientLayer()
+                {
+                    StartPoint = new CGPoint(0, 0.5),
+                    EndPoint = new CGPoint(1, 0.5)
+                };
+            }
+            else
+            {
+                gradientLayer = new CAGradientLayer()
+                {
+                    StartPoint = new CGPoint(0.5, 1),
+                    EndPoint = new CGPoint(0.5, 0)
+                };
+            }
+
+            gradientLayer.Frame = rect;
+            gradientLayer.Colors = new CGColor[]
+            {
+                startColor,
+                endColor
+            };
+
+            NativeView.Layer.InsertSublayer(gradientLayer, 0);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            SetNeedsDisplay();
+        }
+    }
+}
